82b30fa46a47574b66ba8c74f8d330d76d1b5750
M shyam preasad <106077425+kapi272@users.noreply.github.com>
2025-10-21 11:10:29 +0530

    Delete Step1 directory

--- Step1/Lec1:Things_to_know_in_c++/test.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <limits>
-
-int main() {
-    int minInt = std::numeric_limits<int>::min();
-    std::cout << "Minimum value of int: " << minInt << std::endl;
-    return 0;
-}
